ea4b39a5199f9899949f2cbdd9cd6ee65338cc65
Christopher Paliga <krzysztof.paliga@iohk.io>
2021-06-01 08:38:26 +0200

    ETCM-890 load on e2e

---
+++ performance/caliper/networks/e2e/run_passive.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ../../caliper-benchmarks
+for i in {0..4}
+do
+npx caliper launch manager --caliper-benchconfig ../networks/e2e/config.yaml --caliper-networkconfig ../networks/e2e/networkconfig-passive-$i.json --caliper-workspace . &
+done
