64eaed3ae1261d068aca57c9853d3a7a6f4a193d
Christopher Paliga <krzysztof.paliga@iohk.io>
2021-06-24 11:47:30 +0200

    erc20 pr sagano miner

--- pullrequest/run_tests.sh
+++ pullrequest/run_tests.sh
@@ -8,7 +8,7 @@ npx hardhat --network sagano test test/token/ERC20/ERC20.test.js
 
 export SKIPPED=$(cat mochawesome-report/mochawesome.json| egrep -io 'skipped": [0-9]+' | awk '{print $2}')
 echo "Skipped tests count: $SKIPPED"
-if [ $SKIPPED -gt 45 ]; exit 3; fi
+if [ $SKIPPED -gt 45 ];then exit 3; fi
 export FAILED=$(cat mochawesome-report/mochawesome.json | grep -i "fail.: true" | wc -l)
 echo "Failed tests count: $FAILED"
 export TIMED_OUT=$(cat mochawesome-report/mochawesome.json | grep -i timeout | grep -i message | grep -i exceeded | wc -l)
