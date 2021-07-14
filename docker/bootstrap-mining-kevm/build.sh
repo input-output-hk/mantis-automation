1789795e4c2b3db5245fe2f1755eba3349b231c6
Christopher Paliga <krzysztof.paliga@iohk.io>
2021-07-14 11:17:29 +0200

    ETCM-1016 WIP

--- docker/bootstrap-mining-kevm/build.sh
+++ docker/bootstrap-mining-kevm/build.sh
@@ -4,6 +4,14 @@ cd "$(dirname "$0")"
 echo "Copying kevm-vm"
 cp ../../nomad/provisioning/kevm/kevm-vm .
 
+echo "Copuing dag"
+cd ../../nomad/persistance/dag
+cat dag.tar.gz.* | tar xzvf -
+ls -alsh
+cp -r full-R23-0000000000000000 ../../../docker/bootstrap-mining-kevm/ 
+
+cd ../../../docker/bootstra-mining-kevm
+
 for ((i=1;i<=5;i++)); 
 do 
   echo "Processing bootstrap-mining-kevm-${i}"
@@ -19,3 +27,6 @@ done
 
 echo "Removing kevm-vm"
 rm kevm-vm
+
+echo "Removing dag"
+rm full-R23-0000000000000000
