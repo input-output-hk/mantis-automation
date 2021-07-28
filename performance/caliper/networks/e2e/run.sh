5edfd9e11c639699a6e1e1d6a6dcec172451c845
Christopher Paliga <krzysztof.paliga@iohk.io>
2021-07-28 11:33:37 +0200

    ETCM-1062-configurable-load-e2e

--- performance/caliper/networks/e2e/run.sh
+++ performance/caliper/networks/e2e/run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 cd ../../caliper-benchmarks
-for i in {0..0}
-do
-#npx caliper launch manager --caliper-benchconfig ../networks/e2e/config.yaml --caliper-networkconfig ../networks/e2e/networkconfig-miner-$i.json --caliper-workspace . &
-npx caliper launch manager --caliper-benchconfig ../smartcontract/hello/config.yaml --caliper-networkconfig ../networks/e2e/networkconfig-miner-$i.json --caliper-workspace . 
-sleep 600
-done
+npx caliper launch manager --caliper-benchconfig ../smartcontract/hello/config.yaml --caliper-networkconfig ../networks/e2e/networkconfig.json --caliper-workspace . 
