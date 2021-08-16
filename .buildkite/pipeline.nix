3f7af31ae67399100f8fa7d3b4adfbb3cd08e4cd
Christopher Paliga <krzysztof.paliga@iohk.io>
2021-08-16 10:29:59 +0200

    ETCM-1085-load-sagano-nix WIP adds nix

---
+++ .buildkite/pipeline.nix
@@ -0,0 +1,58 @@
+{ cfg, pkgs, ... }:
+
+with cfg.steps.commands;
+let 
+	commonAttrs = {
+		agents.queue = "e2e.mantis.ws";
+	};
+in
+{
+	steps.commands = {
+		prepareCaliper = commonAttrs // {
+			label = "prepare caliper";
+			command = ''
+				cd performance/caliper
+				./prepare.sh
+			'';
+			retry.automatic = false;
+		};
+
+		prepareSmartContractSetup = commonAttrs // {
+			label = "prepare Smart Contract setup";
+			dependsOn = [ prepareCaliper ];
+			command = ''
+				cd smartcontract/hello
+				npm install
+			'';
+			retry.automatic = false;
+		};
+
+		prepareTestRun = commonAttrs //{
+			label = "prepate Test Run";
+			dependsOn = [ prepareSmartContractSetup ];
+			command = ''
+				cd ../../networks/sagano
+				./prepare_run.sh
+			'';
+			retry.automatic = false;
+		};
+
+		setupAccount = commonAttrs // {
+			label = "setup Account";
+			dependsOn = [ prepareTestRun ];
+			command = ''
+				./setup_account.sh
+			'';
+			retry.automatic = false;
+		};
+
+		runTest = commonAttrs // {
+			label = "run test";
+			dependsOn = [ setupAccount ];
+			command = ''
+				./run.sh
+			'';
+			retry.automatic = false;
+		};
+	};
+}
